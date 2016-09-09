59df5b43e533b07cde58872f0dd77ec9f09057cd
Yasmine Lee <yaslee3@yahoo.com>
2016-09-08 21:48:10 -0400

    Initial beginning of commit

---
+++ src/FactorialNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	//add a main to execute the code we write within it
+    public static void main(String[] args) {
+    	
+    	//initializing the instance variables used in the
+		// application
+    	
+    	int num = 0;
+		long factorial = 1;
+		String choice = "y";
+
+
+		
+		// Get input from user
+				Scanner scan = new Scanner(System.in);
+				
+				// welcome message for the user
+				System.out.println("Welcome to the Factorial Calculator!");
+				
+				
+				
+
+	}
+
+}
